2985db3f0dd54bc8f99ad79ca55051ce4a88db89
Chris Insignares <insignaresc115@gmail.com>
2021-12-10 07:01:09 -0500

    update, no new work

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
 
 const app = express();
@@ -14,3 +14,21 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+// ----------------------------------
+// const express = require("express");
+// const path = require("path");
+// const { env } = require("process");
+// const { clog } = require("./middlewares/clog");
+// const sequelize = require("./config/connection");
+// const mysql = require("mysql2");
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// sequelize.sync().then(() => {
+//   app.listen(PORT, () => console.log("Now listening"));
+// });
